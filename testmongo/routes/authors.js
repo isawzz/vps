01d4895b45dc23bab08f38352753b454a93e315c
isawzz <iwgajam@gmail.com>
2022-11-29 06:51:10 -0800

    das

--- testmongo/routes/authors.js
+++ testmongo/routes/authors.js
@@ -1,9 +1,60 @@
-const express = require('express')
+const express = require('express');
 const router = express.Router()
+const Author = require('../models/author');
 
-router.get('/', (req, res) => { res.render('authors/index'); })
-router.get('/new', (req, res) => { res.render('authors/new'); })
-router.post('/', (req, res) => { res.send('Create'); })
+router.get('/', async (req, res) => {
+	//v2: (mit search)
+	let searchOptions = {}
+	if (req.query.name){
+		searchOptions.name = new RegExp(req.query.name,'i')
+	}
+	try {
+		const authors = await Author.find(searchOptions);
+		res.render('authors/index', { authors: authors, author: req.query });
+	} catch {
+		console.log('ERROR!', req.body)
+		res.redirect('/');
+	}
+
+	//v1:
+	// try {
+	// 	const authors = await Author.find();
+	// 	res.render('authors/index', { authors: authors });
+	// } catch {
+	// 	console.log('ERROR!', req.body)
+	// 	res.redirect('/');
+	// }
+
+	//v0:
+	//res.render('authors/index'); 
+})
+router.get('/new', (req, res) => {
+	res.render('authors/new', { author: new Author() });
+})
+router.post('/', async (req, res) => {
+	const author = new Author({ name: req.body.name });
+
+	//v2 (async):
+	try {
+		const o = await author.save();
+		res.redirect('authors'); //res.redirect(`authors/${o.id}`); 
+	} catch {
+		console.log('ERROR!', req.body.name)
+		res.render('authors/new', { author: author, errorMessage: "error creating author!" })
+	}
+
+	//v1:
+	// author.save((err, o) => {
+	// 	if (err) {
+	// 		res.render('authors/new', { author: author, errorMessage: "error creating author!" })
+	// 	} else {
+	// 		res.redirect('authors'); //res.redirect(`authors/${o.id}`);  
+	// 	}
+	// })
+
+	//v0:
+	//res.send(req.body.name);
+})
 
 module.exports = router;
 
--- testmongo/server.js
+++ testmongo/server.js
@@ -1,10 +1,8 @@
-require('dotenv').config();
-console.log('NODE_ENV',process.env.NODE_ENV)
-console.log('db is',process.env.DATABASE_URL)
 
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
+const bodyParser = require('body-parser')
 
 const indexRouter = require('./routes/index')
 const authorsRouter = require('./routes/authors')
@@ -17,17 +15,19 @@ app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({limit: '10mb', extended: false}))
 
-// const mongoose = require('mongoose')
-// mongoose.connect(process.env.DATABASE_URL,{})
-// const db = mongoose.connection
-// db.on('error',x=>console.log(x))
-// db.once('open',()=>console.log('connected to mongoose'))
+const { DATABASE_URL, PORT } = require('../conf')
+const mongoose = require('mongoose')
+mongoose.connect(DATABASE_URL, {})
+const db = mongoose.connection
+db.on('error', x => console.log(x))
+db.once('open', () => console.log('connected to mongoose'))
 
 app.use('/', indexRouter);
 app.use('/authors', authorsRouter); //NOTE: path is composed together with paths in ./routes/authors.js
 
-app.listen(process.env.PORT)
+app.listen(PORT)
 
 
 
