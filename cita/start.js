ed7bd534b3eada2be2c33590fde2fbbed77ecfd8
isawzz <iwgajam@gmail.com>
2022-12-04 15:04:27 -0800

    das

--- cita/start.js
+++ cita/start.js
@@ -3,3 +3,33 @@ onload=start;
 async function start(){
 	await load_syms();
 }
+
+function onclick_code_new(){
+
+}
+function onclick_code_home(){ onclick_code_list(); }
+function onclick_code_list(){
+	//so jetzt brauch ich ein fetch zum server!
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
