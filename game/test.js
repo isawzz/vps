85ed02cffa6dead04eb60368954c59a24651e471
isawzz <iwgajam@gmail.com>
2023-02-22 16:19:59 -0800

    das

--- game/test.js
+++ game/test.js
@@ -1,3 +1,33 @@
+
+function test16b() {
+	let d = document.body; mClass(d, 'fullpage airport');addDummy(d);
+	let areas = [
+		'dTestButtons dTestButtons',
+		'dSearch dSidebar',
+		'dFiddle dSidebar',
+		'dTable dSidebar',
+	];
+	let cols = '1fr 200px';
+	let rows = 'auto auto auto 1fr';
+
+	let [bg, fg] = [rColorTrans(50, 10, 100, [150, 230]), 'contrast']; //console.log('colors:', bg, fg);	//bg='hsla(120,100%,25%,0.3)';
+	dPage = mGridFrom(d, areas, cols, rows, { padding: 4, box: true, bg: bg, fg: fg });
+
+	let elem = mSearch('keywords:', mySearch, dSearch, {}, { selectOnClick: true });
+	let bs = mDiv(elem, { 'grid-column': '1 / span 3', display: 'flex', gap: 4 });
+	mButton('name', onclickFulltext, bs, { align: 'center', w: 110 });
+	mButton('insensitive', onclickCase, bs, { align: 'center', w: 210 });
+	mButton('anywhere', onclickWhere, bs, { align: 'center', w: 210 });
+
+	mStyle(dFiddle, { h: 400 });
+	mDom(dFiddle, {}, { html: 'Edit Code:' });
+	AU.ta = mDom(dFiddle, { fz: 18, family: 'consolas', w100: true, box: true, h: 'rest', bg: colorTrans(bg, 1), fg: 'black' }, { tag: 'textarea', id: 'ta', className: 'plain' });
+
+	mFlex(dTestButtons);
+	mButton('TEST', onclickTest, dTestButtons); //mDom(dTestButtons, { bg: bg, hpadding: 10, vpadding: 4, rounding: 8, cursor: 'pointer' }, { onclick: onclickTest, className: 'hop1', html: 'TEST' });
+
+	mDom(dTable,{},{html:'HAAAAAAAAAALLLLLLLLLOOOOOO',editable:true,selectOnClick:true})
+}
 function test16a() {
 	let d = document.body; mClass(d, 'fullpage airport');addDummy(d);
 	let areas = [
